03d150c28214e8334c4844437d1e24a7e534716e
Galvin <77013913+GalvinPython@users.noreply.github.com>
2022-12-21 15:59:06 +0000

    Initial commit

---
+++ DeleteJavaCheck/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace DeleteJavaCheck;
+
+internal class Program
+{
+    private static string f;
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("DeleteJavaCheck Running");
+
+        String[] lines = File.ReadAllLines(@"location.txt");
+        for (int i = 0; i < lines.Length; i++)
+        {
+            if (lines[i][..1] != "#")
+            {
+                f = (lines[i])+"\\JavaCheck.jar";
+                if (File.Exists(f))
+                {
+                    File.Delete(f);
+                    Console.WriteLine("File deleted!");
+                } else
+                {
+                    Console.WriteLine("File doesn't exist, and couldn't be deleted.");
+                }
+            }
+        }
+
+        Console.WriteLine("Program completed. Press enter to terminate app");
+        Console.ReadKey();
+    }
+}
